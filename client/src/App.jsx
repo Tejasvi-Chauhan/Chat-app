22f08b392412b87baa43c58eb718292cd8604e4e
Tejasvi-Chauhan <chauhantejasvi07@gmail.com>
2026-01-21 23:23:46 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<h1>Home Page</h1>} />
+          <Route path="/about" element={<h1>About Page</h1>} />
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
