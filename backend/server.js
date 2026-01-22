f9ffbb083bf27ce4547f4f3b7878d987fcad2645
Tejasvi-Chauhan <chauhantejasvi07@gmail.com>
2026-01-22 23:35:22 +0530

    new

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const socketio = require('socket.io');
+const cors = require('cors');   
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const http=require("http")
+const server=http.createServer(app)
+const io=socketio(server);
+const router=require("./router")
 
-app.get('/',(req,res)=>{
-    res.send("Server is running");
-})
-app.listen(PORT, () => {
+app.use(cors());
+app.use(router);
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-})
+});
